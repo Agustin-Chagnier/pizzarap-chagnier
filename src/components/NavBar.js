8333dae29421e96f7a06f9ad7feee79195382ba9
Agustin-Chagnier <a.chagnier@gmail.com>
2021-12-07 19:19:05 -0300

    sarasa

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import "./navbar.css";
+
+
+const Navbar = () => {
+    return ( 
+    <nav id="navbar">
+    <div className="titulos">
+    <h1 className="titulo">PizzaRap</h1>
+    </div>
+    <div className="links">
+        <a href="#">COMBOS</a>
+        <a href="#">ESPECIALES</a>
+    </div>
+    </nav>
+
+    )
+}
+
+export default Navbar
