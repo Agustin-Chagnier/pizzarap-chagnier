0b6663c2c2583d7ba37698cea2737ba0bbdaa7f8
Agustin-Chagnier <a.chagnier@gmail.com>
2022-01-20 18:11:16 -0300

    firebase2

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,5 +1,8 @@
 import {Link,NavLink} from "react-router-dom"
 import { useContexto } from "./cartContext.js"
+import { addDoc, collection , serverTimestamp , updateDoc } from "firebase/firestore"
+import { db } from "./firebase"
+/* import Formulario from "./Formulario" */
 
 
 
@@ -8,7 +11,24 @@ const Carrito = () => {
 
     const {carrito,borrarDelCarrito,limpiarCarrito,precio_total} = useContexto()
 
+    const finalizarCompra = () => {
 
+        const ventasCollection = collection(db, "ventas")
+        addDoc(ventasCollection,{
+            cliente : {
+                nombre : "Agustin",
+                apellido : "Chagnier",
+                email : "agustin@mail.com"
+            },
+            items : carrito,
+            fecha : serverTimestamp(),
+            total: precio_total
+        })
+        .then((resultado)=>{
+            console.log(resultado)
+        })
+        limpiarCarrito()
+    }
 
 
     return (
@@ -21,7 +41,7 @@ const Carrito = () => {
                     })}
                     {precio_total}
                     <button onClick={limpiarCarrito}>LIMPIAR</button>
-                    <button>TERMINAR COMPRA</button>
+                    <button onClick={finalizarCompra}>TERMINAR COMPRA</button>
                 </ul>
             ) : <p>No hay productos en el carrito<br/><Link to="/menu">Volver al menu</Link></p>}
         </div>
